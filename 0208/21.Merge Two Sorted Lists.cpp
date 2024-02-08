142ded59b8b3fb7f1b0212f7ee506cb12f52b633
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-02-08 21:58:23 +0800

    Update 21.Merge Two Sorted Lists.cpp

--- 0208/21.Merge Two Sorted Lists.cpp
+++ 0208/21.Merge Two Sorted Lists.cpp
@@ -8,3 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL)
+        {
