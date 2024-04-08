0ee9433aa08eed535635b403df2639626be38601
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-04-08 17:42:10 +0800

    Update 3106. Lexicographically Smallest String After Operations With Constraint.cpp

--- 0408/3106. Lexicographically Smallest String After Operations With Constraint.cpp
+++ 0408/3106. Lexicographically Smallest String After Operations With Constraint.cpp
@@ -2,5 +2,22 @@ class Solution {
 public:
     string getSmallestString(string s, int k) {
         string ans = s;
-       
-        
+        for(char &ch : s)
+        {
+            int left = ch - 'a';
+            int right = 'z' - ch + 1;
+            int mn = min(left,right);
+            if(k >= mn)
+            {
+                k -= mn;
+                ch = 'a';
+            }
+            else 
+            {
+                ch = ch - k;
+                break;
+            }
+        }
+        return s;
+    }
+};
