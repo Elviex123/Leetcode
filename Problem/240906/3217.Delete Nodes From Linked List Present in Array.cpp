660650fb52dce906990c1c31f2ebfcbd17d7f8a7
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-09-06 09:49:37 +0800

    Update 3217.Delete Nodes From Linked List Present in Array.cpp

--- Problem/240906/3217.Delete Nodes From Linked List Present in Array.cpp
+++ Problem/240906/3217.Delete Nodes From Linked List Present in Array.cpp
@@ -54,3 +54,30 @@ No node has value 5.
 
 這個算法的流程是依次檢查並刪除與 `nums` 集合中的值匹配的節點，最終返回刪除後的鏈表。
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int>s;
+        for(auto x:nums) s.insert(x);
+         ListNode* newHead= new ListNode(-1);
+        ListNode* temp=newHead;
+        while(head){
+            if(s.count(head->val)==0){
+                newHead->next=new ListNode(head->val);
+                newHead=newHead->next;
+            }
+            head=head->next;
+        }
+        return temp->next;
+    }
+};
